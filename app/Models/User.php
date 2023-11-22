aa89fe7ad6949aa7fac120722075602404397c13
roelh <roelhoogendoorn01369@gmail.com>
2023-11-22 14:44:10 +0100

    Created Migration & Model files for: Users, Tours, Organisations.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -21,6 +23,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'organisation_id',
+        'profile_photo',
+        'is_admin',
     ];
 
     /**
@@ -42,4 +47,15 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function tours(): HasMany
+    {
+        return $this->hasMany(Tour::class);
+    }
+
+    public function organisation(): BelongsTo
+    {
+        return $this->belongsTo(Organisation::class);
+    }
+
 }
---
+++ database/factories/TourFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Tour;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TourFactory extends Factory
+{
+    protected $model = Tour::class;
+
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+}
