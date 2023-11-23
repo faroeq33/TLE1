d184e67f60442b6f6b29a9af192fd34770258486
Faroeq <alves_1044@hotmail.com>
2023-11-23 15:15:34 +0100

    Add config for styleguide

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ export default {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        fontFamily: {
+            sans: ["Graphik", "sans-serif"],
+            serif: ["Merriweather", "serif"],
+        },
+        extend: {
+            colors: {
+                primary: "#4255ff", // lightened blue
+                secondary: "#1b2266", // darkened blue
+                tertiary: "#fa740e", // foxconnect orange
+                dark: "#36485f", // textbackground
+                light: "#fff", // ligther text for darker backgrounds
+                mute: "rgb(156, 163, 175)", // ligther text for darker backgrounds
+                mutehover: "rgb(107 114 128)",
+            },
+            spacing: {
+                128: "32rem",
+                144: "36rem",
+            },
+        },
     },
     plugins: [],
 };
