4e94e3d4868bf9592cf69d00ff790bd7547111dd
Faroeq <alves_1044@hotmail.com>
2023-11-23 19:04:29 +0100

    Fix nav profile icon & buttons

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -28,13 +28,13 @@
             <div class="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
                 <div class="flex items-center flex-shrink-0 p-4 m-4 ">
                     <a href="{{ route('home') }}">
-                        <p class="font-primary uppercase">IP-car</p>
+                        <p class="uppercase font-primary">IP-car</p>
                     </a>
                 </div>
 
                 <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
                     <a href="{{ route('home') }}"
-                        class="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700">Livestream</a>
+                        class="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-tertiary hover:text-gray-700">Livestream</a>
 
                 </div>
             </div>
@@ -47,8 +47,13 @@
                         id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                         <span class="absolute -inset-1.5"></span>
                         <span class="sr-only">Open user menu</span>
-                        <img class="w-8 h-8 rounded-full" src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                            alt="">
+                        {{-- profile icon --}}
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+
                     </button>
 
 
