12e69dd8f6b7a482bd57be5238010cabc91d6dd0
Faroeq <alves_1044@hotmail.com>
2023-11-29 19:15:10 +0100

    Add rol

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -2,32 +2,37 @@
 
 @section('content')
     <div class="container mx-auto">
-        <h1 class="text-3xl font-bold mb-4">Admin Dashboard</h1>
+        <h1 class="mb-4 text-3xl font-bold">Admin Dashboard</h1>
         <table class="min-w-full bg-white border border-gray-300">
             <thead>
-            <tr>
-                <th class="py-2 px-4 border-b text-center">Name</th>
-                <th class="py-2 px-4 border-b text-center">Email</th>
-                <th class="py-2 px-4 border-b text-center">Organisation</th>
-                <th class="py-2 px-4 border-b text-center">Actions</th>
-            </tr>
+                <tr>
+                    <th class="px-4 py-2 text-center border-b">Name</th>
+                    <th class="px-4 py-2 text-center border-b">Email</th>
+                    <th class="px-4 py-2 text-center border-b">Organisation</th>
+                    <th class="px-4 py-2 text-center border-b">Rol</th>
+                    <th class="px-4 py-2 text-center border-b">Actions</th>
+                </tr>
             </thead>
             <tbody>
-            @foreach($users as $user)
-                <tr>
-                    <td class="py-2 px-4 border-b text-center">{{ $user->name }}</td>
-                    <td class="py-2 px-4 border-b text-center">{{ $user->email }}</td>
-                    <td class="py-2 px-4 border-b text-center">{{ $user->organisation->name ?? '-' }}</td>
-                    <td class="py-2 px-4 border-b text-center">
-                        <a href="{{ route('admin.edit', $user->id) }}" class="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded">Edit</a>
-                        <form method="post" action="{{ route('admin.delete', $user->id) }}" class="inline">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
+                @foreach ($users as $user)
+                    <tr>
+                        <td class="px-4 py-2 text-center border-b">{{ $user->name }}</td>
+                        <td class="px-4 py-2 text-center border-b">{{ $user->email }}</td>
+                        <td class="px-4 py-2 text-center border-b">{{ $user->organisation->name ?? '-' }}</td>
+                        <td class="px-4 py-2 text-center border-b">{{ $user->is_admin ? 'Admin' : 'Gids' }}</td>
+
+                        <td class="px-4 py-2 text-center border-b">
+                            <a href="{{ route('admin.edit', $user->id) }}"
+                                class="px-2 py-1 text-white bg-blue-500 rounded hover:bg-blue-700">Edit</a>
+                            <form method="post" action="{{ route('admin.delete', $user->id) }}" class="inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit"
+                                    class="px-2 py-1 text-white bg-red-500 rounded hover:bg-red-700">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
             </tbody>
         </table>
     </div>
