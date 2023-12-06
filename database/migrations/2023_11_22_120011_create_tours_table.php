c90165f0306c2e4e9d4f94855fa705e560b0abaa
roelh <roelhoogendoorn01369@gmail.com>
2023-12-06 11:52:30 +0100

    Added cascade on delete to tours table.

--- database/migrations/2023_11_22_120011_create_tours_table.php
+++ database/migrations/2023_11_22_120011_create_tours_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('tours', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->dateTime('datetime');
             $table->string('customer', 255);
             $table->string('email', 191);
