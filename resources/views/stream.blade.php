4182e91dd4ace49103bbf3c8092c79408c556bfc
Angelique-Smit <113983273+Angelique-Smit@users.noreply.github.com>
2023-12-20 12:34:07 +0100

    SteeringInstructions (#63)
    * -Added SteeringInstructions.png
    
    * -Uncommented model on index.blade.php so the instructions are visible

--- resources/views/stream.blade.php
+++ resources/views/stream.blade.php
@@ -72,7 +72,7 @@
                         <!-- Modal content -->
                         <div class="modal-content">
                             <span class="close">&times;</span>
-                            <p>Grafiek met hulpzame informatie over de besturing</p>
+                            <img src="{{ Vite::asset('/resources/img/SteeringInstructions.png') }}" alt="Instructions on driving the IP-Car. Hold W to go straight ahead. Hold A to steer to the left and hold D to steer to the right. Hold S to go backwards. Keep in mind that in order to turn, you have to hold both your steering key (A or D) and the W key.">
                         </div>
 
                     </div>
