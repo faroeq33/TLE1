742258ad33f13d52a3c1a0b6f8dc0798a303d62a
Faroeq Alves <11407191+faroeq33@users.noreply.github.com>
2024-01-05 17:00:57 +0100

    Fix for production vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/ip_car_webapp/app.js", // Nodig voor de livestream blade files
+                "resources/css/styles.css", // Nodig voor de livestream blade files
             ],
             refresh: true,
         }),
