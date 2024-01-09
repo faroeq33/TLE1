6e3a36512e001d00854ab6e77184f94753fefc01
Faroeq <alves_1044@hotmail.com>
2024-01-09 13:10:36 +0100

    update viteconfig ipcar/

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 "resources/css/app.css",
                 "resources/js/app.js",
+                "resources/js/ip_car_webapp/stream.js", // Nodig voor de livestream blade files
                 "resources/js/ip_car_webapp/app.js", // Nodig voor de livestream blade files
                 "resources/css/styles.css", // Nodig voor de livestream blade files
             ],
